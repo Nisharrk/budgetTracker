bce2a85d0f8e7ea460a69dcbcba4458f58a82ab8
nisharrk <nishantsharma2633@gmail.com>
2023-02-12 18:32:46 -0500

    Added add budget entry page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import AddBudgetButton from "@/src/components/body/addBudgetEntry";
+import AddBudgetButton from "@/src/components/body/addBudgetButton";
 import BudgetEntries from "@/src/components/body/BudgetEntries";
 
 const Home = () => {
