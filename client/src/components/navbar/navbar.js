ebc222ea67eebc718da2028173be6591e7b8a70e
nisharrk <nishantsharma2633@gmail.com>
2023-02-11 23:42:30 -0500

    help

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="bg-black py-6 text-white p-3 text-xl font-semibold">
+    <div className="bg-black py-6 text-white p-3 text-xl font-semibold border-b border-teal-500">
       Budget Tracker 💸
     </div>
   );
